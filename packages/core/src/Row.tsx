1b1ea5d807be0227ee75275473d2881fd486cbba
InfiniteXyy <xuyiyangwing@gmail.com>
2020-11-03 17:01:35 +0800

    chore: update scripts

--- packages/core/src/Row.tsx
+++ packages/core/src/Row.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 type RowProps = {
-  gap?: number; // default is 1rem
+  gap?: string | number; // default is 1rem
   childMinWidth?: number; // Will display items vertically if cannot satisfy
   grid?: boolean; // Work with childMinWidth, will display like grid
-  overflow?: "shrink" | "wrap" | "hidden" | "scroll" // default is wrap
+  overflow?: 'shrink' | 'wrap' | 'hidden' | 'scroll'; // default is wrap
   inline?: boolean; // work as inline component
-}
+  children?: React.ReactNode;
+};
 export function Row(props: RowProps): JSX.Element {
-  return <div>1</div>;
+  const { children } = props;
+  return <div>{children}</div>;
 }
--- packages/example/src/index.tsx
+++ packages/example/src/index.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Box } from 'just-layout';
+import { Box, Row } from 'just-layout';
 
 function App() {
-  return <Box as="button">happsy</Box>;
+  return (
+    <div>
+      <Row gap="2em">
+        <div>Layout</div>
+        <div>CSS</div>
+        <div>JavaScript</div>
+      </Row>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
